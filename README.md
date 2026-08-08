f07a3c96ca15409c1e829ebe247a1a8ed02f6727
CHuks <techsirchuks@gmail.com>
2026-08-07 20:06:29 -0400

    I added .terraform and co to gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# terra1
+THis is a test repo for github actions 
---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.57.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "fb5254d0-da10-4428-ae41-85602fe2931c"
+}
+
+
+#Here's our resource group
+resource "azurerm_resource_group" "terra1" {
+  name     = "terra1"
+  location = "southafricanorth"
+}
